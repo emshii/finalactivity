261052915051e3c0371f108c17bc30baa134206d
emshii <201880223@psu.palawan.edu.ph>
2021-01-27 20:41:42 +0800

    adding data to server feature implemented

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
- <?php 
-    //connect to mysql server
-    $dbhost = 'localhost';
-    $dbname = 'test';
-    $dbuser = 'root';
-    $dbpass = '';
-    $conn = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+<?php
+    include_once("config.php");
 
-    $result = mysqli_query($conn, "SELECT * FROM users");
+    $result = mysqli_query($mysqli, "SELECT * FROM users");
 ?>
 
 <!DOCTYPE html>
@@ -17,13 +12,15 @@
     <title>Document</title>
 </head>
 <body>
+<a href = "add.html">Add new Data </a><br><br>
     <h1> <?php  echo "PHP CRUD APP"; ?></h1>
     <table>
         <tr bgcolor='#cccccc'>
             <td> Name </td>
             <td> Age </td>
             <td> Email </td>
-            <td> Created </td>  
+            <td> Created </td>
+            <td> Update </td> 
         </tr>
         
 
@@ -35,7 +32,7 @@
                 echo "<td>".$res['age']."</td>";
                 echo "<td>".$res['email']."</td>";
                 echo "<td>".$res['date_created']."</td>";
-
+                echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a>";
                 echo "</tr>";
             }
             
