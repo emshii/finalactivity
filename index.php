745bb92c38a142401fe1cb9a6dc2c352715d7b3e
emshii <201880223@psu.palawan.edu.ph>
2021-01-28 18:42:20 +0800

    Edited a Design

--- index.php
+++ index.php
@@ -21,7 +21,12 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <title>PHP CRUD</title>
     <style>
-        
+        a{
+            color:white;
+        }
+        a:hover{
+            color:black;
+        }
     </style>
 </head>
 <body style = "background-image: url('Back.png')" >
